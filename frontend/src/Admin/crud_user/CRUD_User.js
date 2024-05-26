1938b965c1b3ad3168ec307b8ad5af740199109f
Nguyen Pham <nguyenpham6273@gmail.com>
2024-05-26 22:07:30 +0700

    edit record

--- frontend/src/Admin/crud_user/CRUD_User.js
+++ frontend/src/Admin/crud_user/CRUD_User.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import AdSidebar from '../AdNav/AdSidebar';
+import AdSidebar from '../AdNav/AdSidebar'
 import AdNavbar from '../AdNav/AdNavbar';
 import '@coreui/coreui/dist/css/coreui.min.css';
 import './CRUD_User.css';
